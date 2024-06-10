a05c05747cb2473b34291b1fd7eac1af56b5d996
Tang Hao Liang <tang.hao.liang.thl@gmail.com>
2024-06-11 03:39:17 +0800

    Edit to Hose Reel Tutorial

---
+++ Assets/Scripts/FireHose.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireHose : MonoBehaviour
+{
+    public GameObject hoseValve;
+    public bool completed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hoseValve.gameObject.transform.eulerAngles.y < -170 && !completed) {
+            completed = true;
+            print("Done");
+        }
+    }
+}
