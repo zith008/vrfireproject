7930723fb1ae277d30b73c267500397c68256747
Tang Hao Liang <tang.hao.liang.thl@gmail.com>
2024-06-11 18:45:56 +0800

    Creation of Stop Drop Scene

---
+++ Assets/Scripts/StopDropTutorial.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopDropTutorial : MonoBehaviour
+{
+    public ParticleSystem fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire.Stop();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("MainCamera")) {
+            fire.Stop();
+        }
+    }
+}
